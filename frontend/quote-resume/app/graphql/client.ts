b250ad3ed593808e94ab2d153855c2c8efb42fe1
Elkeno Jones <elkeno.jones@binarystar.ca>
2026-05-20 11:44:16 -0300

    add logging and telemetry to frontend

--- frontend/quote-resume/app/graphql/client.ts
+++ frontend/quote-resume/app/graphql/client.ts
@@ -1,17 +1,27 @@
-import { InMemoryCache } from "@apollo/client";
+import { ApolloLink, InMemoryCache } from "@apollo/client";
 import { HttpLink } from "@apollo/client";
 import { ApolloClient } from "@apollo/client";
+import { errorLink, traceLink } from "~/logging/apollo";
+
+const localLink = new HttpLink({
+  uri: "http://localhost:4000/graphql",
+});
+const rickMLink = new HttpLink({
+  uri: "https://rickandmortyapi.com/graphql",
+});
 
 export const getClient = () => new ApolloClient({
-  link: new HttpLink({
-    uri: "http://localhost:4000/graphql",
-  }),
+  link: ApolloLink.from([localLink, traceLink, errorLink]),
+  // link: new HttpLink({
+  //   uri: "http://localhost:4000/graphql",
+  // }),
   cache: new InMemoryCache()
 });
 
 export const getRickMortyClient = () => new ApolloClient({
-  link: new HttpLink({
-    uri: "https://rickandmortyapi.com/graphql",
-  }),
+  // link: new HttpLink({
+  //   uri: "https://rickandmortyapi.com/graphql",
+  // }),
+  link: ApolloLink.from([rickMLink]),
   cache: new InMemoryCache()
 })
---
+++ frontend/quote-resume/app/logging/apollo.ts
@@ -0,0 +1,54 @@
+import { ApolloLink, Observable } from "@apollo/client";
+import { ErrorLink } from "@apollo/client/link/error";
+import { v4 as uuidv4 } from "uuid";
+
+export const traceLink: ApolloLink | undefined = new ApolloLink((operation, forward) => {
+  const traceId = uuidv4();
+
+  operation.setContext(({ headers = {} }) => ({
+    headers: {
+      ...headers,
+      "x-trace-id": traceId,
+    },
+  }));
+
+  return new Observable((observer) => {
+    const subscription = forward(operation).subscribe({
+      next: (response) => {
+        fetch(import.meta.env.VITE_OO_ENDPOINT, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: import.meta.env.VITE_OO_AUTH,
+          },
+          body: JSON.stringify({
+            traceId,
+            operationName: operation.operationName,
+            timestamp: new Date().toISOString(),
+          }),
+        });
+
+        return response;
+      },
+      error: (err) => observer.error(err),
+      complete: () => observer.complete()
+    });
+
+    return () => {
+      if (subscription) subscription.unsubscribe();
+    }
+  })
+});
+
+export const errorLink = new ErrorLink(({ error, operation }) => {
+  fetch(import.meta.env.VITE_OO_ENDPOINT, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      errors: error || operation,
+      timestamp: new Date().toISOString(),
+    }),
+  });
+});
